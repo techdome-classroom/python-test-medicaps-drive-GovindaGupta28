b80b388310f9bea87ba4211ac26c3c01079e65d6
GovindaGupta28 <ag6420564@gmail.com>
2024-04-25 13:53:39 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,7 +1,27 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+def romanToInt(s: str) -> int:
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    prev_value = 0
+
+    for char in s:
+        current_value = roman_values[char]
+        if current_value > prev_value:
+            result += current_value - 2 * prev_value
+        else:
+            result += current_value
+        prev_value = current_value
+
+    return result
+
+# Test cases
+print(romanToInt("III"))      # Output: 3
+print(romanToInt("LVIII"))    # Output: 58
+print(romanToInt("MCMXCIV"))
